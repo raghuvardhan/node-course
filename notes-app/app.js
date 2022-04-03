0883f13e7d469089c2497283597fd7df02404637
Raghuvardhan Karanam <karanamraghuvardhan12@gmail.com>
2022-04-03 09:09:56 +0530

    add gitignore

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,5 @@
-const validator = require('validator');
+const chalk = require('chalk');
 const notes = require('./notes.js');
 
 const note = notes();
 console.log(note);  
-
-console.log(validator.isURL('node'));
